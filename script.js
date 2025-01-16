a30b675ef9fa5e0d497059ff3439634823fc4ac6
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-16 20:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let age=document.getElementById('age');
+let uname=document.getElementById('name');
+let btn=document.getElementById('btn');
+// console.log(name,name);
+btn.addEventListener('click',(e)=>{
+
+  e.preventDefault();
+  // console.log(age.value);
+  // console.log(uname.value);
+	let a=age.value;
+
+	a=+a;
+	let b=uname.value;
+  // console.log(a,b);
+	if(!a || !b){
+		return alert('insert all fields are required');
+	}
+	age.value='';
+	uname.value='';
+	return new Promise((res,rej)=>{
+		if(a>=18){
+			setTimeout(()=>{
+				res(alert(`Welcome, ${b}. You can vote.`))
+			},4000)
+		}else{
+			setTimeout(()=>{
+			rej(alert(`Oh sorry ${b}. You aren't old enough.`));
+			},4000);
+				
+		}
+	});
+})
